0c17e2172bf529583c91dac4bc2c39d765c15819
EnriqueLLatasFlores <102637887+EnriqueLLatasFlores@users.noreply.github.com>
2024-11-20 15:13:39 -0500

    Update WebConfig.java

--- src/main/java/upc/edu/oneup/configs/WebConfig.java
+++ src/main/java/upc/edu/oneup/configs/WebConfig.java
@@ -14,17 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
-            }
-        }
-/*        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Permite todas las rutas
-                        .allowedOrigins("*","http://localhost:5173") // Permite todos los orígenes
-                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Permite estos métodos
-                        .allowedHeaders("*"); // Permite todos los encabezados
-            }*/
+                        .allowedOrigins("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS");
+            };
         };
+
     }
 
+
+};
