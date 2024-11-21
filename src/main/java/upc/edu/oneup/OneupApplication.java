a9f84c9c142c6cc48bffaee98844976d70c8a618
EnriqueLLatasFlores <102637887+EnriqueLLatasFlores@users.noreply.github.com>
2024-11-21 00:01:15 -0500

    Update OneupApplication.java

--- src/main/java/upc/edu/oneup/OneupApplication.java
+++ src/main/java/upc/edu/oneup/OneupApplication.java
@@ -10,4 +10,20 @@ public class OneupApplication {
 		SpringApplication.run(OneupApplication.class, args);
 	}
 
+	  @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite todas las rutas
+                        .allowedOrigins("*", "http://localhost:5173") // Permite todos los orígenes
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Permite estos métodos
+                        .allowedHeaders("*"); // Permite todos los encabezados
+            }
+
+
+
+        };
+
+    }
 }
