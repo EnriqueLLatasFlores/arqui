87e3d4f3f40194b0ea8645a1c577791c71bfd5cb
EnriqueLLatasFlores <102637887+EnriqueLLatasFlores@users.noreply.github.com>
2024-11-21 00:05:06 -0500

    Update OneupApplication.java

--- src/main/java/upc/edu/oneup/OneupApplication.java
+++ src/main/java/upc/edu/oneup/OneupApplication.java
@@ -2,28 +2,31 @@ package upc.edu.oneup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class OneupApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OneupApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OneupApplication.class, args);
+    }
 
-	  @Bean
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Permite todas las rutas
-                        .allowedOrigins("*", "http://localhost:5173") // Permite todos los orígenes
-                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Permite estos métodos
-                        .allowedHeaders("*"); // Permite todos los encabezados
+                        .allowedOrigins(
+                                "https://elegant-love-production.up.railway.app", // Dominio de producción
+                                "http://localhost:4200" // Dominio de desarrollo
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                        .allowedHeaders("*") // Permite todos los encabezados
+                        .allowCredentials(true); // Permite el uso de cookies o credenciales
             }
-
-
-
         };
-
     }
 }
