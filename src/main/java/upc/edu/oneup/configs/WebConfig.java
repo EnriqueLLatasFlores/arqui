2929842b04a41a36417be7aa979d39821fca321b
EnriqueLLatasFlores <102637887+EnriqueLLatasFlores@users.noreply.github.com>
2024-11-21 06:37:50 -0500

    Update WebConfig.java

--- src/main/java/upc/edu/oneup/configs/WebConfig.java
+++ src/main/java/upc/edu/oneup/configs/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Permite todos los endpoints
-                        .allowedOrigins("*") // Permite todas las URLs de origen
+                        .allowedOrigins("elegant-love-production.up.railway.app") // Permite todas las URLs de origen
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
