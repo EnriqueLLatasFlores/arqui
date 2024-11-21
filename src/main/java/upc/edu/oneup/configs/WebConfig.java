6abf0ecbe1be4ee3ec25ceb1e07e3793b63fa77e
EnriqueLLatasFlores <102637887+EnriqueLLatasFlores@users.noreply.github.com>
2024-11-20 23:36:28 -0500

    Update WebConfig.java

--- src/main/java/upc/edu/oneup/configs/WebConfig.java
+++ src/main/java/upc/edu/oneup/configs/WebConfig.java
@@ -13,12 +13,16 @@ public class WebConfig implements WebMvcConfigurer {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")  // Permite todas las rutas
-                        .allowedOrigins("http://localhost:5173", "https://charming-purpose-production.up.railway.app") // Orígenes permitidos
-                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
-                        .allowedHeaders("*") // Encabezados permitidos
-                        .allowedCredentials(true); // Permite credenciales como cookies y cabeceras de autenticación
+                registry.addMapping("/**") // Permite todas las rutas
+                        .allowedOrigins("*", "http://localhost:5173",""http://localhost:5173","**","https://charming-purpose-production.up.railway.app") // Permite todos los orígenes
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Permite estos métodos
+                        .allowedHeaders("*"); // Permite todos los encabezados
             }
+
+
+
         };
+
     }
 }
+
